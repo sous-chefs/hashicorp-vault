c760847fe55ab8170d49e56d870bb2855f9afdc1
John Bellone <jbellone@bloomberg.net>
2015-06-09 18:04:20 -0400

    Adds integration tests using serverspec.

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,5 +1,24 @@
 require 'spec_helper'
 
+describe command('which vault') do
+  its(:exit_status) { should eq 0 }
+  its(:stdout) { should match '/usr/local/bin/vault' }
+end
+
+describe group('vault') do
+  it { should exist }
+end
+
+describe user('vault') do
+  it { should exist }
+end
+
+describe file('/etc/vault.json') do
+  it { should be_file }
+  it { should be_mode '0644' }
+end
+
 describe service('vault') do
+  it { should be_enabled }
   it { should be_running }
 end
