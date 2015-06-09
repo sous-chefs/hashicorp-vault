93a5ca14875af2cb8948544c60acf9234268c945
John Bellone <jbellone@bloomberg.net>
2015-06-08 20:21:06 -0400

    Adds support for writing out configuration as json.

--- libraries/vault_config.rb
+++ libraries/vault_config.rb
@@ -11,7 +11,7 @@ class Chef::Resource::VaultConfig < Chef::Resource
   include Poise(fused: true)
   provides(:vault_config)
 
-  attribute(:config_filename, kind_of: String, name_attribute: true)
+  attribute(:filename, kind_of: String, name_attribute: true)
   attribute(:user, kind_of: String, default: 'vault')
   attribute(:group, kind_of: String, default: 'vault')
 
@@ -26,11 +26,45 @@ class Chef::Resource::VaultConfig < Chef::Resource
   attribute(:backend_type, equal_to: %i{consul zookeeper inmem file}, default: :inmem)
   attribute(:backend_options, options_collector: true)
 
-  action(:create) do
+  def tls?
+    !tls_disable
+  end
 
+  def to_json
+    invalid_options = %i{filename user group backend_type backend_options}
+    config = to_hash.reject { |k, v| invalid_options.include?(k) }
+    JSON.pretty_generate(config.merge(backend_type => backend_options), quicks_mode: true)
+  end
+
+  action(:create) do
+    # TODO: (jbellone) Perhaps we break this out and use chef-vault to
+    # seed the initial secrets configuration here? I do not like the
+    # idea of specifying this outside of the HWRP.
+    if new_resource.tls?
+      fail 'TLS certificate file must be set!' if new_resource.tls_cert_file.empty?
+      fail 'TLS key file must be set!' if new_resource.tls_key_file.empty?
+    end
+
+    notifying_block do
+      directory ::File.dirname(new_resource.filename) do
+        recursive true
+        mode '0644'
+      end
+
+      file new_resource.filename do
+        content new_resource.to_json
+        owner new_resource.user
+        group new_resource.group
+        mode '0640'
+      end
+    end
   end
 
   action(:delete) do
-
+    notifying_block do
+      file new_resource.filename do
+        action :delete
+      end
+    end
   end
 end
