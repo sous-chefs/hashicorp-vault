2e4b90f1f2e7729938adc52b7544304a8d610f5d
John Bellone <jbellone@bellone.us>
2016-04-19 08:17:23 -0400

    Swap the order of install versus config.

--- recipes/default.rb
+++ recipes/default.rb
@@ -9,6 +9,12 @@ poise_service_user node['hashicorp-vault']['service_user'] do
   not_if { node['hashicorp-vault']['service_user'] == 'root' }
 end
 
+install = vault_installation node['hashicorp-vault']['version'] do |r|
+  if node['hashicorp-vault']['installation']
+    node['hashicorp-vault']['installation'].each_pair { |k, v| r.send(k, v) }
+  end
+end
+
 config = vault_config node['hashicorp-vault']['config']['path'] do |r|
   owner node['hashicorp-vault']['service_user']
   group node['hashicorp-vault']['service_group']
@@ -17,12 +23,6 @@ config = vault_config node['hashicorp-vault']['config']['path'] do |r|
     node['hashicorp-vault']['config'].each_pair { |k, v| r.send(k, v) }
   end
   notifies :reload, "vault_service[#{node['hashicorp-vault']['service_name']}]", :delayed
-end
-
-install = vault_installation node['hashicorp-vault']['version'] do |r|
-  if node['hashicorp-vault']['installation']
-    node['hashicorp-vault']['installation'].each_pair { |k, v| r.send(k, v) }
-  end
 end
 
 vault_service node['hashicorp-vault']['service_name'] do |r|
