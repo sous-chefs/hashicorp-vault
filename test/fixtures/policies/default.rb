2b199d564a40c3881e5557bec328e00b438671cb
John Bellone <jbellone@bellone.us>
2016-07-19 07:51:01 -0400

    Update project to use docker/travis for integration tests.

---
+++ test/fixtures/policies/default.rb
@@ -0,0 +1,9 @@
+name 'default'
+default_source :community
+default_source :chef_repo, '..'
+cookbook 'hashicorp-vault', path: '../../..'
+run_list 'hashicorp-vault::default'
+named_run_list :centos, 'yum::default', run_list
+named_run_list :debian, 'apt::default', run_list
+named_run_list :freebsd, 'freebsd::default', run_list
+named_run_list :windows, 'windows::default', run_list
