514b2c92ac48ceaccd5436f13637388c6538a9ca
Tim Smith <tsmith@chef.io>
2018-09-09 12:19:55 -0700

    Disable FC121 in the metadata
    Signed-off-by: Tim Smith <tsmith@chef.io>

--- metadata.rb
+++ metadata.rb
@@ -15,7 +15,8 @@ supports 'centos', '>= 6.4'
 supports 'windows'
 supports 'freebsd'
 
-depends 'build-essential'
+# build-essential is obsolete in chef 14+
+depends 'build-essential' # ~FC121
 depends 'golang', '~> 1.7'
 depends 'poise', '~> 2.6'
 depends 'poise-service', '~> 1.1'
