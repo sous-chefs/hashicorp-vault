8fc74aeddb6482dd67a89a21f79082dbb0c39e9d
rickzhang <rickzhang@roblox.com>
2024-09-05 22:50:38 -0700

    add mingw to default list

--- test/fixtures/policies/default.rb
+++ test/fixtures/policies/default.rb
@@ -2,4 +2,5 @@ name 'default'
 default_source :supermarket
 default_source :chef_repo, '..'
 cookbook 'hashicorp-vault', path: '../../..'
+cookbook 'mingw', '2.1.1'
 run_list 'hashicorp-vault::default'
