f2e52d40d57a94e19f517d455e254d5525d2b1ef
rickzhang <rickzhang@roblox.com>
2024-09-05 22:44:13 -0700

    add vault data symlink

--- test/unit/libraries/vault_service_spec.rb
+++ test/unit/libraries/vault_service_spec.rb
@@ -12,9 +12,16 @@ describe VaultCookbook::Resource::VaultService do
 
   before do
     stub_command('getcap /opt/vault/0.9.1/vault|grep cap_ipc_lock+ep').and_return(false)
+
+     # Create the /data directory to simulate the mount
+     allow(File).to receive(:directory?).with('/data').and_return(true)
   end
 
   context 'with default properties' do
     it { is_expected.to run_execute 'setcap cap_ipc_lock=+ep /opt/vault/0.9.1/vault' }
+
+    #Test for symlink creation
+    it 'creates a symlink for /var/log/vault to /data/var/log/vault' do
+      expect(chef_run).to create_link('/var/log/vault').with(to: '/data/var/log/vault')
   end
 end
